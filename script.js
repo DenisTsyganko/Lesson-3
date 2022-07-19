4823ad8bf63385abdf8bba3ef72448a4fb1e9023
Denis <tsyganko_denis@mail.ru>
2022-07-19 16:26:32 +0300

    Updated for lesson4

--- script.js
+++ script.js
@@ -12,6 +12,81 @@ function start() {
     }
 }
 
+
+start();
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses : {},  
+    income : [],
+    savings : true,
+    chooseExpenses: function(){
+        for (let i = 0; i < 2; i++){
+            let answers = askTwoQuestions("Введите обязательную статью расходов в этом месяце",
+            "Во сколько обойдется?");
+            appData.expenses[answers[0]] = answers[1];
+        }
+    },
+    calculateDailyBudget: function(){
+        appData.dailyBudget = Math.round(appData.budget / 30);
+        alert(`Your daily budget is ${appData.dailyBudget}`);
+    },
+    detectLevel: function(){
+        if(appData.dailyBudget < 100){
+            console.log("Low income");
+        } else if (appData.dailyBudget >= 100 && appData.dailyBudget < 2000){
+            console.log("Average budget");
+        } else if (appData.dailyBudget > 2000){
+            console.log("Good income");
+        } else {
+            console.log("Some error occured!");
+        }
+    },
+    checkSavings: function(){
+        if (appData.savings){
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент,");
+            appData.monthIncome = save/100/12*percent;
+            alert(`Доход в месяц с вашего депозита: ${appData.monthIncome} `);  
+        }  
+    },
+    chooseOptExpenses: function(){
+        for (let i = 0; i < 3; i++){
+            let answers = askTwoQuestions("Введите необязательную статью расходов в этом месяце",
+            "Во сколько обойдется?");
+            appData.optionalExpenses[answers[0]] = answers[1];
+        }
+    },
+    chooseIncome: function(){
+        let items = prompt("Что принесет дополнительный доход? (перечислите через запятую)");
+        while(items == null || !isNaN(items) || items === ''){
+            items = prompt("Введите корректные данные.Что принесет дополнительный доход?" +
+            " (перечислите через запятую)");
+        }
+        appData.income = items.split(',');
+        appData.income.push(prompt('Может что-то еще?'));
+        appData.income.sort();
+        appData.income.forEach(element => {
+            alert("Способы доп. заработка: " + element);
+        });
+    }
+};
+
+appData.chooseExpenses();
+appData.calculateDailyBudget();
+appData.detectLevel();
+appData.checkSavings();
+appData.chooseOptExpenses();
+appData.chooseIncome();
+console.log(appData);
+for (let element in appData){
+    console.log(`Наша программа включает в себя данные: ${element}`);
+}
+
+
+
+
 // Asking user two questions and return data. First answer has to be string, second number
 function askTwoQuestions(questionOne, questionTwo){
     let answerOne = prompt(questionOne);
@@ -30,65 +105,3 @@ function askTwoQuestions(questionOne, questionTwo){
     }
     return [answerOne, answerTwo];
 }
-
-// Asking user for expenses and saving them to appData.expenses object
-function chooseExpenses(){
-    for (let i = 0; i < 2; i++){
-        let answers = askTwoQuestions("Введите обязательную статью расходов в этом месяце",
-        "Во сколько обойдется?");
-        appData.expenses[answers[0]] = answers[1];
-    }
-}
-
-// Calculating daily budget and displaying information about income to console
-function calculateDailyBudget(){
-    appData.dailyBudget = Math.round(appData.budget / 30);
-    alert(`Your daily budget is ${appData.dailyBudget}`);
-}
-// Displaying information regarding income level of user to console
-function detectLevel(){
-    if(appData.dailyBudget < 100){
-        console.log("Low income");
-    } else if (appData.dailyBudget >= 100 && appData.dailyBudget < 2000){
-        console.log("Average budget");
-    } else if (appData.dailyBudget > 2000){
-        console.log("Good income");
-    } else {
-        console.log("Some error occured!");
-    }
-}
-// Checking whether user has any savings and requesting additional info from user
-function checkSavings(){
-    if (appData.savings){
-        let save = +prompt("Какова сумма накоплений?"),
-            percent = +prompt("Под какой процент,");
-
-        appData.monthIncome = save/100/12*percent;
-        alert(`Доход в месяц с вашего депозита: ${appData.monthIncome} `);  
-    }
-}
-
-// Asking user regarding optional epenses for next month and saving them to appData object
-function chooseOptExpenses(){
-    for (let i = 0; i < 3; i++){
-        let answers = askTwoQuestions("Введите необязательную статью расходов в этом месяце",
-        "Во сколько обойдется?");
-        appData.optionalExpenses[answers[0]] = answers[1];
-    }
-}
-
-start();
-let appData = {
-    budget : money,
-    timeData : time,
-    expenses : {},
-    optionalExpenses : {},  
-    income : [],
-    savings : true,
-}
-chooseExpenses();
-calculateDailyBudget();
-detectLevel();
-checkSavings();
-chooseOptExpenses();
-console.log(appData);
